96aa03a3b929842d2e9ec7d682f9c50798c47aec
Youssef <yf.marzouk@gmail.com>
2024-11-17 18:52:54 +0100

    Initial Commit

---
+++ netflix/netflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflixApp.swift
+//  netflix
+//
+//  Created by Youssef on 17/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct netflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
